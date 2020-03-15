531bb7a067abb43907c18c8eeb76068b72532d7b
Rob Wells <robert.wells@colorado.edu>
2020-03-14 20:26:53 -0600

    Update 3

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ for i in range(1,sizer[0]):
         country_list.append(namer)
 
 print(np.size(country_list))
-print(country_list)
 num_countries = np.size(country_list)
 country_rates = np.zeros((num_countries,sizer[1]-4))
 for i in range(num_countries):
@@ -40,43 +39,81 @@ for i in range(num_countries):
             for k in range(4,sizer[1]):
                 country_rates[i,k-4] += float(confirmed_data[j][k])
 
-wanted = ["China","US","Italy","Germany","Korea, South"]  # ["Italy","US","Germany","Korea, South"]  
+wanted = ["China","US","Italy"]  # ["Italy","US","Germany","Korea, South"]  
 wanted_pop = [1401710720.,329431067.,60252824.,83149300.,51780579.]
 wanted_pop_density = [418.,200.,200.,233.,517.]
+wanted_index = np.zeros(np.size(wanted))
+
+wanted_fit_param = np.zeros((np.size(wanted),3))
 
 # actual China pop density 145, but will use an average of south, east and central
 # actual US pop density 34, but will use number for city pop density
 
-
-fig, ax = plt.subplots()
+#====================================== solve for parameters ======================================#
 for i in range(num_countries):
-    #if country_rates[i,-1] > 800:
     if country_list[i] in wanted:
         num = wanted.index(country_list[i])
-        #fig, ax = plt.subplots()
+        wanted_index[num] = i
         legend = country_list[i]
-        if 1==0:  # actual dates
-            x = np.array(range(sizer[1]-4)) ; print(np.size(x)) ; x = np.ma.masked_equal(x,0.0) ; print(np.size(x))
-            y = np.array(country_rates[i,:]) ; y = np.ma.masked_equal(y,0.0)
+        for j in range(np.size(country_rates[i,:])):  # set start time to be at > 200 infections
+            if country_rates[i,j] > 200.:
+                val = j
+                break
+        x = np.array(range(np.size(country_rates[i,:])-j)) ; y = np.array(country_rates[i,j:])
+        popt = tl.fit_function(pl.logistic,x,y/wanted_pop_density[num],legend)
+        wanted_fit_param[num,:] = popt
+#==================================================================================================#
+
+print(wanted_index)
+print(wanted_fit_param)
+
+#wanted_fit_param[:,0] = wanted_fit_param[0,0]
+wanted_fit_param[1:,1] = wanted_fit_param[0,1]/3.
+#wanted_fit_param[:,2] = wanted_fit_param[0,2]
+
+
+fig, ax = plt.subplots()
+for i in range(np.size(wanted)):
+        legend = wanted[i]
         if 1==1: # shift time to where day 0 has > 100 confirmed cases
             for j in range(np.size(country_rates[i,:])):
                 if country_rates[i,j] > 200.:
                     val = j
                     break
-            x = np.array(range(np.size(country_rates[i,:])-j)) ; y = np.array(country_rates[i,j:])
-        pl.scatter_plot(x,(y),['Confirmed Cases','Days','Num Affected',legend],'.')
-        #pl.scatter_fit_plot(pl.logistic,(x),(y),['Confirmed Cases','Days','Num Affected (log10)',legend])
-        #pl.scatter_fit_plot(pl.logistic,(x),(y)/wanted_pop_density[num],['Confirmed Cases','Days','Num Affected per Population Density',legend])
-        popt = tl.fit_function(pl.logistic,x,y/wanted_pop_density[num],legend)
-        num_sim = 60
-        modelPredictions = pl.logistic((range(num_sim)), *popt)
-        #pl.scatter_plot(range(num_sim),modelPredictions,['Confirmed Cases','Days','Num Affected',legend],'-')
-        pl.scatter_plot(range(num_sim),modelPredictions*wanted_pop_density[num],['Confirmed Cases','Days','Num Affected',legend],'-')
-
-        #x = np.array(range(sizer[1]-4)) ;  trim_size = np.size(np.trim_zeros(x)) ; x = x[:trim_size]
-        #y = np.array(country_rates[i,:]) ; print(y) ; y = y[np.size(y)-trim_size:] ; print(y)
+            x = np.array(range(np.size(country_rates[wanted_index[i],:])-j)) ; y = np.array(country_rates[wanted_index[i],j:])
+        pl.scatter_plot(x,(y)/wanted_pop_density[i],['Confirmed Cases','Days','Num Affected',legend],'.')
+        num_sim = 40
+        modelPredictions = pl.logistic((range(num_sim)), *wanted_fit_param[num,:])   # *wanted_pop_density[num]
+        pl.scatter_plot(range(num_sim),modelPredictions,['Confirmed Cases','Days','Num Affected',legend],'-')
 
 plt.show()
+
+
+if 1==0:
+    for i in range(num_countries):
+        #if country_rates[i,-1] > 800:
+        if country_list[i] in wanted:
+            num = wanted.index(country_list[i])
+            legend = country_list[i]
+            if 1==0:  # actual dates
+                x = np.array(range(sizer[1]-4)) ; print(np.size(x)) ; x = np.ma.masked_equal(x,0.0) ; print(np.size(x))
+                y = np.array(country_rates[i,:]) ; y = np.ma.masked_equal(y,0.0)
+            if 1==1: # shift time to where day 0 has > 100 confirmed cases
+                for j in range(np.size(country_rates[i,:])):
+                    if country_rates[i,j] > 200.:
+                        val = j
+                        break
+                x = np.array(range(np.size(country_rates[i,:])-j)) ; y = np.array(country_rates[i,j:])
+            #pl.scatter_plot(x,(y)/wanted_pop_density[num],['Confirmed Cases','Days','Num Affected',legend],'.')
+            #pl.scatter_fit_plot(pl.logistic,(x),(y),['Confirmed Cases','Days','Num Affected (log10)',legend])
+            #pl.scatter_fit_plot(pl.logistic,(x),(y)/wanted_pop_density[num],['Confirmed Cases','Days','Num Affected per Population Density',legend])
+            popt = tl.fit_function(pl.logistic,x,y/wanted_pop_density[num],legend)
+            wanted_fit_param[num,:] = popt
+            num_sim = 40
+            modelPredictions = pl.logistic((range(num_sim)), *popt)   # *wanted_pop_density[num]
+            #pl.scatter_plot(range(num_sim),modelPredictions,['Confirmed Cases','Days','Num Affected',legend],'-')
+
+    #plt.show()
 
 
 if 1==0:
